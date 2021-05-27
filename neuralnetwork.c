ee77ff0e1dadebb7b424d537e148dc99be4e1b67
Muhamad Aldiansyah <muhamadaldiansyah24@gmail.com>
2021-05-27 19:27:11 +0800

    Update neuralnetwork.c

--- neuralnetwork.c
+++ neuralnetwork.c
@@ -3,28 +3,7 @@
 #include <time.h>
 #include <stdlib.h>
 
-/*
-	neuralnetwork.c
-	train the data use this code, for classification 
-
-	original code:
-		Neural Network PC Tools by Russell C. Eberhart and Roy W. Dobbins
-
-	edited by: 
-		Muhamad Aldiansyah
-	
-	This original code is taken from:
-	Neural Network PC Tools by Russell C. Eberhart and Roy W. Dobbins
-	
-	The implementation from the book is using command line interface (cli).
-	I edited the code so it can running in IDE like Eclipse for C.
-	
-		
-*/
-
-
 int main(){
-
 
 	/* setting neural network */
 	int nIterations = 1500;
