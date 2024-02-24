5fdb4a1deb7ca97cf912eda1b53431479fdaaa77
Matt Aldi <muhamadaldiansyah24@gmail.com>
2024-02-25 01:07:27 +0800

    Update neuralnetwork_test_satu.c

--- neuralnetwork_test_satu.c
+++ neuralnetwork_test_satu.c
@@ -3,24 +3,6 @@
 #include <time.h>
 #include <stdlib.h>
 
-/*
-	neuralnetwork_test_satu.c
-	test one from all the data, for classification task.
-	
-	original code:
-		Neural Network PC Tools by Russell C. Eberhart and Roy W. Dobbins
-
-	edited by: 
-		Muhamad Aldiansyah
-	
-	This original code is taken from:
-	Neural Network PC Tools by Russell C. Eberhart and Roy W. Dobbins
-	
-	The implementation from the book is using command line interface (cli).
-	I edited the code so it can running in IDE like Eclipse for C.
-		
-*/
-
 
 float find_maximum(float[], float);
 
